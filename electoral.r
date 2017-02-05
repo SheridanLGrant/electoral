c9b406347dcf514f7259086fc505dea9a0102843
Sheridan Grant <sheridanlgrant@gmail.com>
2017-02-04 20:28:15 -0800

    This is annoying

--- electoral.r
+++ electoral.r
@@ -3,6 +3,7 @@
 # 12/24/2016
 
 require(zoo)
+require(xlsx)
 
 setwd("C:/Users/Sheridan/Desktop/Code/Electoral")
 data <- read.csv("2000.csv", na.strings="")
@@ -20,8 +21,11 @@ for (i in 1:dim(data)[1]) {
   data$votePerc[i] <- data$voteTotal[i] / data[data$state == data$state[i] & data$demo == 'Total', 3]
   data$elec[i] <- data$votePerc[i] * alloc$X1991[match(data$state[i], alloc$State)]
 }
+data$elecRate <- data$elec / data$voteTotal
 
 for (demo in unique(data$demo)) {
   cat(demo, 'electoral votes per thousand people:', 
       sum(data[data$demo == demo,]$elec) / sum(data[data$demo == demo,]$voteTotal), "\n")
 }
+
+2k4 <- read.xlsx("2004.xls", 2)
